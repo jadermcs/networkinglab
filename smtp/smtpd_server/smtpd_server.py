6ba6b8044334807807114783cfe881ebe22c193e
Jader Martins <jadermcs94@gmail.com>
2018-04-21 10:07:47 -0300

    ems

--- smtp/smtpd_server/smtpd_server.py
+++ smtp/smtpd_server/smtpd_server.py
@@ -8,10 +8,10 @@ box = "emailbox"
 class CustomSMTPServer(smtpd.SMTPServer):
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        print('Receiving message from:', peer)
-        print('Message addressed from:', mailfrom)
-        print('Message addressed to  :', rcpttos)
-        print('Message length        :', len(data))
+        print('[DEBUG] Receiving message from:', peer)
+        print('[DEBUG] Message addressed from:', mailfrom)
+        print('[DEBUG] Message addressed to  :', rcpttos)
+        print('[DEBUG] Message length        :', len(data))
         filename = "{}.eml".format(datetime.now().strftime('%Y%m%d%H%M%S'))
         for rcp in rcpttos:
             ruser = box + '/' + rcp.split("@")[0]
